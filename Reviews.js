1ebc0b247f03da430435be676d864a24b8c55fb7
Karl Zakhary <karlmaged33@gmail.com>
2017-04-03 15:36:27 +0200

    First Commit

---
+++ Reviews.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var ReviewSchema = new mongoose.Schema({
+    author: String,
+    body: String,
+    company: [{type: mongoose.Schema.Types.ObjectId, ref:'Company'}],
+    upvotes: {type: Number, default: 0}
+    }
+) ;
+
+ReviewSchema.methods.upvote = function(cb) {
+  this.upvotes += 1;
+  this.save(cb);
+};
+
+ReviewSchema.methods.downvote = function(cb) {
+  this.upvotes -= 1;
+  this.save(cb);
+};
+
+mongoose.model('Review', ReviewSchema);
