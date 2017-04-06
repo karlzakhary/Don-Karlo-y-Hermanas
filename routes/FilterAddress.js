ac72277b6a92b5d83cf17c01d2cd833d538182fd
Mahmoud Mowafy <m.mowafy.11@gmail.comgit config --global user.email m.mowafy.11@gmail.com>
2017-04-06 18:32:00 +0200

    My work so far

---
+++ routes/FilterAddress.js
@@ -0,0 +1,19 @@
+var searchAddress = module.exports = {
+router.get('/search/byAddress', function(req,res){
+
+     var SearchItem=req.query.SearchItem;
+ 
+
+        var Company = mongoose.model('Company');
+
+        Company.find({businessAddress: SearchItem},function(err,result){
+          if(err){
+            res.json(err);
+          }
+          else{
+            res.json(result);
+          }
+        });
+
+});
+};
